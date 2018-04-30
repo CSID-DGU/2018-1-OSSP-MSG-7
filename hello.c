d76cf55b89d43b912f99a3473c5b631e7e0b854b
Seung Woo <tmddn645@naver.com>
2018-04-30 21:38:39 +0900

    helpers(오픈소스) 추가, AirPlane헤더 구현해 놓은 cpp 추가했음

--- hello.c
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main()
-{
-	int i= 0;
-	std::cout<<"hello";
-	return 0;
-}
--- 연습용/AirPlane.h
+++ 연습용/AirPlane.h
@@ -1,7 +1,4 @@
 #include "helpers.h"
-#include <vector>
-
-using namespace std;
 
 class AirPlane
 {
@@ -9,18 +6,40 @@ private:
   SDL_Surface *plane;
   SDL_Surface *bullet;
   int pos_x,pos_y;// 비행기 x,y 좌표;
-
+  int life;
 public:
   vector<SDL_Rect> bullet_rect;
 
   AirPlane();
   ~AirPlane();
-  //void Got_shot(발사체 좌표, 너비, 길이);
+  bool Got_shot(vector<SDL_Rect> enemy_bullets);
   void shooting();
   void bullet_apply_surface(SDL_Surface* destination, SDL_Rect* clip);
   void plane_apply_surface(SDL_Surface* destination, SDL_Rect* clip);
   void control_bullet();
   void control_plane(int x, int y);
+  SDL_Surface *Get_plane();//plane 변수 getter
+  vector<SDL_Rect> Get_bullets();//총알 변수 getter
+};
+
+class Enemy_standard
+{
+private:
+  SDL_Surface *enemy;
+  SDL_Surface *bullets;
+  int pos_x,pos_y;// 비행기 x,y 좌표;
+  int life;
+public:
+  vector<SDL_Rect> bullet_rects;
+
+  Enemy_standard();
+  ~Enemy_standard();
+  bool Got_shot(vector<SDL_Rect> bullets);
+  void shooting();
+  void bullet_apply_surface(SDL_Surface* destination, SDL_Rect* clip);
+  void enemy_apply_surface(SDL_Surface* destination, SDL_Rect* clip);
+  void control_bullet();
+  void control_plane(int x, int y);
   SDL_Surface *Get_plane();
-  //vector<SDL_Surface*> Get_bullets();
+  vector<SDL_Rect> Get_bullets();
 };
